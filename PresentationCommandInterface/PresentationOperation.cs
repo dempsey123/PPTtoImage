fa8d7294ed985e3123422d3e076fd4710b2193d0
dempsey123 <qqq532651525@gmail.com>
2018-06-19 00:19:21 +0800

    another way to transfer ppt to image

--- PresentationCommandInterface/PresentationOperation.cs
+++ PresentationCommandInterface/PresentationOperation.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using System.IO;
 using Syncfusion.Presentation;
 using Syncfusion.OfficeChartToImageConverter;
-using System.Drawing;
+using System.Drawing; // IMPORTANT: Import System.Drwing libarary.
+using Syncfusion.UI;
+using System.Drawing.Imaging;
 
 namespace PresentationCommandInterface
 {
@@ -16,7 +18,7 @@ namespace PresentationCommandInterface
         public PresentationOperation()
         {
             var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string filePath = Path.Combine(documents, @"AIMobileOffice\MSPowerPoint\presentation.pptx");
+            string filePath = Path.Combine(documents, @"presentation.pptx");
             //string filePath = @"C:\Users\Wentao Ruan\Downloads\AIPPT_Main.pptx";
 
             // Open a presentation at a specified file path. Set file mode to be ReadWrite mode.
@@ -27,22 +29,42 @@ namespace PresentationCommandInterface
 
         public void GotoImage()
         {
-            presentation.ChartToImageConverter = new ChartToImageConverter();
-
-            presentation.ChartToImageConverter.ScalingMode = Syncfusion.OfficeChart.ScalingMode.Best;
-
-            Image[] images = presentation.RenderAsImages(Syncfusion.Drawing.ImageType.Metafile);
-
             var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string fileDir = Path.Combine(documents, @"AIMobileOffice/MSPowerPoint");
             Directory.CreateDirectory(fileDir);
             string filePath = Path.Combine(documents, @"AIMobileOffice/MSPowerPoint");
-            int i = 0;
-            foreach (Image image in images)
+
+            int customWidth = 5000;
+            int customHeight = 2800;
+
+            for (int j = 0; j < presentation.Slides.Count; j++)
             {
-                image.Save(filePath + i.ToString());
-                i++;
+                Stream stream = presentation.Slides[j].ConvertToImage(Syncfusion.Drawing.ImageFormat.Bmp);
+                Bitmap bitmap = new Bitmap(customWidth, customHeight, PixelFormat.Format32bppPArgb);
+
+                Graphics graphic = Graphics.FromImage(bitmap);
+                bitmap.SetResolution(graphic.DpiX, graphic.DpiY);
+
+                graphic.DrawImage(System.Drawing.Image.FromStream(stream), new Rectangle(0, 0, bitmap.Width, bitmap.Height));
+
+                bitmap.Save(filePath + j.ToString());
             }
+
+            presentation.Close();
+
+           // presentation.ChartToImageConverter = new ChartToImageConverter();
+
+          //  presentation.ChartToImageConverter.ScalingMode = Syncfusion.OfficeChart.ScalingMode.Best;
+
+           // Image[] images = presentation.RenderAsImages(Syncfusion.Drawing.ImageType.Metafile); //convert format (size)
+
+           
+            //int i = 0;
+            //foreach (Image image in images)
+            //{
+            //    image.Save(filePath + i.ToString());
+            //    i++;
+            //}
         }
     }
 }
